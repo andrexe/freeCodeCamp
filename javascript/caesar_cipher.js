760970cb79514d77b1e3b7839331421d575b1c3f
Andrexe <andrei.alexe@outlook.com>
2022-01-22 11:32:39 +0200

    Caesar cipher
    Added solution for Caesar cipher exercise

--- javascript/caesar_cipher.js
+++ javascript/caesar_cipher.js
@@ -17,10 +17,38 @@ rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the strin
 
 */
 
-// 
+// Find the character code range for letters to do the conversion
+// Split the string into an array and transform each element into its character code equivalent
+// Since we're decoding, if the char code is one of the LAST 13, we will be subtracting 13 from each code
+// If the character code is one of the FIRST 13, we are adding 13 (because there are 26 letters)
+// If needed, do the same for lowercase character code range
+// Convert the character codes back into their corresponding letters
+// Join the array back into a string and return the result
 
 function rot13(str) {
-    return str;
+    // Create the result array by
+    let result = str
+    // Splitting up the string into invidual elements
+        .split('')
+        // Translating each character into its equivalent code
+        .map(l => l.charCodeAt(0))
+        // Decoding each character using the function defined below
+        .map(decode);
+
+    function decode(elem) {
+        // If the character is between A -> M, add 13 and turn it into N -> Z
+        if (elem >= 65 && elem <= 77) {
+            elem += 13;
+        } else
+        // ELSE if the character is between N -> Z, subtract 13 and turn it into A -> M
+            if (elem >= 78 && elem <= 90) {
+                elem -= 13;
+            }
+        // Return the decoded character
+        return elem;
+    };
+    // Return the string equivalent of the array with decoded characters
+    return String.fromCharCode(...result);
 }
 
-rot13("SERR PBQR PNZC");
+console.log(rot13("AM SERR PBQR PNZC NZ"));
