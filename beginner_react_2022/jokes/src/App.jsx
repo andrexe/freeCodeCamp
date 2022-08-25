21a49e4ff23d48a166929a1007635986aa7c16d1
Andrei <andrei.alexe@outlook.com>
2022-08-25 23:18:28 +0300

    Beginner React 2022 - 5. Jokes

---
+++ beginner_react_2022/jokes/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import Joke from "./components/Joke"
+
+function App() {
+
+  return (
+    <div className="p-6">
+      <Joke
+        setup="What do you call a factory that makes okay products?"
+        punchline="A satisfactory."
+        isPun={true}
+        upvotes={10}
+        comments={[
+          {
+            author: "Joe",
+            body: "I can't get enough of these jokes",
+            title: "Too funny"
+          }
+        ]}
+      />
+      <Joke
+        setup=""
+        punchline="Dear Math, grow up and solve your own problems."
+        isPun={false}
+        upvotes={10}
+        comments={[
+          {
+            author: "John",
+            body: "I don't understand these jokes",
+            title: "Is it funny?"
+          },
+          {
+            author: "Joe",
+            body: "I can't get enough of these jokes",
+            title: "Too funny"
+          }
+        ]
+      }
+      />
+      <Joke
+        setup="What did the janitor say when he jumped out of the closet?"
+        punchline="Supplies!"
+        isPun={true}
+        upvotes={6}
+        comments={[
+          {
+            author: "Jimothy",
+            body: "I don't like these jokes",
+            title: "Not funny"
+          }
+        ]}
+      />
+      <Joke
+        setup=""
+        punchline="I only know 25 letters of the alphabet. I don't know y."
+        isPun={true}
+        upvotes={5}
+        comments={[
+          {
+            author: "James",
+            body: "I love these jokes",
+            title: "Soo funny"
+          }
+        ]}
+      />
+      <Joke
+        setup="What did one wall say to the other?"
+        punchline="I'll meet you at the corner."
+        isPun={false}
+        upvotes={4}
+        comments={[
+          {
+            author: "Jim",
+            body: "I am going to steal these jokes",
+            title: "Very funny"
+          }
+        ]}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ beginner_react_2022/jokes/src/components/Joke.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Joke = (props) => {
+    // console.log(props);
+    return (
+        <section>
+            <details open>
+                <summary className='text-2xl font-bold'>{props.setup ? props.setup : props.punchline}</summary>
+                {props.setup && <p className='text-xl'>{props.punchline}</p>}
+            </details>
+            <section className='p-2'>
+                <h3 className='text-lg'>Comments</h3>
+                {props.comments.map(comment => {
+                    console.log(comment.title);
+                    return <section className='p-2'>
+                        <small>{comment.author}</small>
+                        <h5 className='font-bold'>{comment.title}</h5>
+                        <p>{comment.body}</p>
+                    </section>
+                })}
+                {/* author
+                title
+                body */}
+            </section>
+        </section>
+    )
+}
+
+export default Joke
