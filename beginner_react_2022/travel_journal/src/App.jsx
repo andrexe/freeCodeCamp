80872d3bb7e1f5234297a21f820b7c05d76e5262
Andrei <andrei.alexe@outlook.com>
2022-08-27 00:33:53 +0300

    Beginner React 2022 - 6.1 Travel Journal fixes
    Fixed image sourcing and cards layout

--- beginner_react_2022/travel_journal/src/App.jsx
+++ beginner_react_2022/travel_journal/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   })
 
   return (
-    <div className='p-6 flex flex-col gap-4'>
+    <div className='p-6 flex flex-col gap-10'>
       {cards}
     </div>
   )
--- beginner_react_2022/travel_journal/src/components/card.jsx
+++ beginner_react_2022/travel_journal/src/components/card.jsx
@@ -4,8 +4,8 @@ import pin from '../img/pin.png'
 const card = (props) => {
     return (
         <section className='max-w-xl flex flex-col sm:flex-row mx-auto'>
-            <img className='rounded-lg object-cover aspect-[3/1] sm:aspect-[11/16] sm:max-h-60' src="https://source.unsplash.com/photos/WLxQvbMyfas" alt="" />
-            <section className='flex flex-col gap-1 pt-2 sm:p-6'>
+            <img className='rounded-lg object-cover aspect-[2/1] sm:aspect-[11/16] sm:max-h-60' src={props.imageUrl} alt="" />
+            <section className='flex flex-col gap-1 pt-2 sm:px-6'>
                 <div className='flex gap-2 items-center'>
                     <img src={pin} alt="" className='w-3' />
                     <small className='uppercase tracking-widest'>{props.location}</small>
