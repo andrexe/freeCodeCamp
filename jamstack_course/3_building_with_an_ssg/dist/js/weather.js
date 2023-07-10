1e9077aadd062dbb3602c96e63e7ccfec468bce5
Andrei <andrei.alexe@outlook.com>
2023-07-10 19:19:10 +0300

    Jamstack course - fix typo in weather.js

--- jamstack_course/3_building_with_an_ssg/dist/js/weather.js
+++ jamstack_course/3_building_with_an_ssg/dist/js/weather.js
@@ -1,6 +1,6 @@
 function success(pos) {
 
-    const url = `/.netlify/functions//weatherapi?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&units=metric`;
+    const url = `/.netlify/functions/weatherapi?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&units=metric`;
 
     fetch(url)
         .then((response) => {
